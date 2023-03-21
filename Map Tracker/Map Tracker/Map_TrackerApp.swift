40428c91a0ed46f60884151ada28c863fde7e392
Faisal Atif <faisal@Faisals-MacBook-Pro-2.local>
2023-03-21 14:40:34 -0700

    added the empty xcode project and the documentation folder

---
+++ Map Tracker/Map Tracker/Map_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_TrackerApp.swift
+//  Map Tracker
+//
+//  Created by Faisal Atif on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct Map_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# MapTracker
-# MapTracker
