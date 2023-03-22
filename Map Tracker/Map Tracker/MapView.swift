9370644bf4adaeda85828adfba91a6f50f88cd78
jadche <jad.charbatji@gmail.com>
2023-03-22 06:37:47 +0300

    Tried adding markers, not showing up. Is real data being taken?

--- Map Tracker/Map Tracker/MapView.swift
+++ Map Tracker/Map Tracker/MapView.swift
@@ -59,28 +59,94 @@ import CoreLocation
 //    }
 //}
 
+//struct MapView: UIViewRepresentable {
+//    var route: [CLLocationCoordinate2D]
+//
+//    //Creates an instance of 'MKMapView', sets its delegate to the coordinator and returns it.
+//    func makeUIView(context: Context) -> MKMapView {
+//        let mapView = MKMapView()
+//        mapView.showsUserLocation = true // Enable showing user's current location
+//        mapView.delegate = context.coordinator
+//
+//        // Set a fixed region to display
+//        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), latitudinalMeters: 500, longitudinalMeters: 500)
+//        mapView.setRegion(region, animated: false)
+//
+//        return mapView
+//    }
+//
+//    // Updates the map view when the route binding changes
+//    // It removes all existing overlays, creates a new polyline with the route's coordinates, and adds it as an overlay to the map view.
+//    func updateUIView(_ uiView: MKMapView, context: Context) {
+//        uiView.removeOverlays(uiView.overlays)
+//        let polyline = MKPolyline(coordinates: route, count: route.count)
+//        uiView.addOverlay(polyline)
+//    }
+//
+//    // Creates an instance of the coordinator class and returns it
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//    // The Coordinator class is used to customize the appearance of the overlay.
+//    class Coordinator: NSObject, MKMapViewDelegate {
+//        var parent: MapView
+//
+//        init(_ parent: MapView) {
+//            self.parent = parent
+//        }
+//
+//        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+//            if let polyline = overlay as? MKPolyline {
+//                let renderer = MKPolylineRenderer(polyline: polyline)
+//                //Set stoke color and line width
+//                renderer.strokeColor = UIColor.red
+//                renderer.lineWidth = 4
+//                return renderer
+//            }
+//            return MKOverlayRenderer(overlay: overlay)
+//        }
+//    }
+//}
+
+//adding markers
+
 struct MapView: UIViewRepresentable {
     var route: [CLLocationCoordinate2D]
 
-    //Creates an instance of 'MKMapView', sets its delegate to the coordinator and returns it.
+    // Creates an instance of 'MKMapView', sets its delegate to the coordinator and returns it.
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        mapView.showsUserLocation = true // Enable showing user's current location
+        mapView.showsUserLocation = true
         mapView.delegate = context.coordinator
-        
-        // Set a fixed region to display
         let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), latitudinalMeters: 500, longitudinalMeters: 500)
         mapView.setRegion(region, animated: false)
-        
         return mapView
     }
 
     // Updates the map view when the route binding changes
-    // It removes all existing overlays, creates a new polyline with the route's coordinates, and adds it as an overlay to the map view.
+    // It removes all existing overlays and annotations, creates a new polyline with the route's coordinates, and adds it as an overlay to the map view.
     func updateUIView(_ uiView: MKMapView, context: Context) {
         uiView.removeOverlays(uiView.overlays)
+        uiView.removeAnnotations(uiView.annotations)
+
         let polyline = MKPolyline(coordinates: route, count: route.count)
         uiView.addOverlay(polyline)
+
+        // Add start and end markers
+        if let startLocation = route.first {
+            let startMarker = MKPointAnnotation()
+            startMarker.coordinate = startLocation
+            startMarker.title = "Start"
+            uiView.addAnnotation(startMarker)
+        }
+
+        if let endLocation = route.last {
+            let endMarker = MKPointAnnotation()
+            endMarker.coordinate = endLocation
+            endMarker.title = "End"
+            uiView.addAnnotation(endMarker)
+        }
     }
 
     // Creates an instance of the coordinator class and returns it
@@ -99,12 +165,25 @@ struct MapView: UIViewRepresentable {
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
             if let polyline = overlay as? MKPolyline {
                 let renderer = MKPolylineRenderer(polyline: polyline)
-                //Set stoke color and line width
                 renderer.strokeColor = UIColor.red
                 renderer.lineWidth = 4
                 return renderer
             }
             return MKOverlayRenderer(overlay: overlay)
         }
+
+        // Customize the appearance of the annotation views
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            if annotation is MKUserLocation {
+                // Use the default blue dot for the user's current location
+                return nil
+            }
+
+            let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            annotationView.pinTintColor = UIColor.blue
+            annotationView.canShowCallout = true
+            return annotationView
+        }
     }
 }
+
